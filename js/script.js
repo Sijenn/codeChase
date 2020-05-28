a66433f2343497846ee06358f55cb0e070903400
jenny <jenn7nm@gmail.com>
2020-05-28 00:11:38 -0400

    add resetPassword function

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function passwordReset(){
+    $('form.reset-password-form').on('submit', function(e){
+        e.preventDefault();
+        $('.reset-form').removeClass('d-block').addClass('d-none');
+
+        $('.reset-confirmation').addClass('d-block');
+    })
+}
+window.addEventListener('load', function(){
+    passwordReset();
+})
